e7bd4f53c006ff88409d4fa8b3d85a3e472e9cc6
Ayan Atal <ayanatal.me19@rvce.edu.in>
2020-09-04 13:58:56 +0530

    Updates

---
+++ precache-manifest.ef43143434cf325cae03f41b4e191a09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f71f9306bb259fa1446e94b7140646",
+    "url": "/reacit-calculator/index.html"
+  },
+  {
+    "revision": "77b7a6e49d6621372012",
+    "url": "/reacit-calculator/static/css/main.9a081441.chunk.css"
+  },
+  {
+    "revision": "5beab37c699d9fb10a2e",
+    "url": "/reacit-calculator/static/js/2.5e72f31d.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/reacit-calculator/static/js/2.5e72f31d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77b7a6e49d6621372012",
+    "url": "/reacit-calculator/static/js/main.0f5e9b1c.chunk.js"
+  },
+  {
+    "revision": "03346561c8434ee02282",
+    "url": "/reacit-calculator/static/js/runtime-main.a34b0f7c.js"
+  },
+  {
+    "revision": "b11b3b21445211ae090886b2d4a72cf5",
+    "url": "/reacit-calculator/static/media/calc2.b11b3b21.jpg"
+  }
+]);
